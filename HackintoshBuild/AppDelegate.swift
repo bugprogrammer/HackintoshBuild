a76eea8776fe428f043b577f1b6e277c39088253
Arabaku <Arabaku@126.com>
2020-01-21 17:27:41 +0800

    [Add]魔改软件更新模块

--- HackintoshBuild/AppDelegate.swift
+++ HackintoshBuild/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-@NSApplicationMain
+//@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
---
+++ HackintoshBuild/main.swift
@@ -0,0 +1,75 @@
+//
+//  main.swift
+//  HackintoshBuild
+//
+//  Created by 刘靖禹 on 2020/1/21.
+//  Copyright © 2020 Arabaku. All rights reserved.
+//
+
+/**
+ 适配版本更新
+ */
+
+import Foundation
+import Cocoa
+
+fileprivate class LaunchTool {
+    
+    fileprivate func launchMyAppFromPath(path: String) {
+        let cmd = "open -n \(path)"
+        self.runCommand(cmd: cmd)
+    }
+    
+    @discardableResult
+    fileprivate func runCommand(cmd: String) -> String? {
+        MyLog("run command: \(cmd)")
+        
+        let task = Process()
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", cmd]
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        
+        let output = String(data: data, encoding: .utf8)
+        task.waitUntilExit()
+        return output
+    }
+    
+}
+
+fileprivate let argc = CommandLine.argc
+fileprivate let argv = CommandLine.unsafeArgv
+fileprivate let arguments = CommandLine.arguments
+
+let needUpadte = UserDefaults.standard.bool(forKey: "update")
+
+if needUpadte {
+    UserDefaults.standard.set(false, forKey: "update")
+    
+    if arguments.count < 2 {
+        exit(1)
+    }
+    
+    let targetPath = arguments[1]
+    let downloadedPath = arguments[2]
+
+    let myFileManager = FileManager.default
+    
+    guard myFileManager.fileExists(atPath: targetPath) && myFileManager.fileExists(atPath: downloadedPath) else {
+        exit(1)
+    }
+    
+    let tool = LaunchTool()
+    let rmCmd = "rm -rf \(targetPath)"
+    tool.runCommand(cmd: rmCmd)
+    let mvCmd = "mv \(downloadedPath) \(targetPath)"
+    tool.runCommand(cmd: mvCmd)
+    tool.launchMyAppFromPath(path: targetPath)
+    
+    exit(0)
+}
+
+_ = NSApplicationMain(argc, argv)
